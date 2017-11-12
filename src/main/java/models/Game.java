4c5c32c4306eae6168b71568072d742d64574394
Nicholas Nelson <nelsonni@oregonstate.edu>
2017-11-11 22:15:13 -0800

    partial refactor of sprint3

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -1,60 +1,25 @@
 package models;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
 
-/**
- * Assignment 1: Students must implement dealFour(), remove(), move(), and columnHasCards() methods
- *
- * The customDeal() method is not present in the Assignment1_Student version since tests (and the test dir) are removed
- * to prevent confusion regarding testing and the use of unit tests; testing is covered more thoroughly in CS362.
- */
 public class Game {
 
-    public java.util.List<Card> deck = new ArrayList<>();
-
-    public java.util.List<java.util.List<Card>> cols = new ArrayList<>();
-
+    public Deck deck = new Deck();
+    public java.util.List<Column> columns = new ArrayList<>();
 
     public Game(){
-        cols.add(new ArrayList<Card>());
-        cols.add(new ArrayList<Card>());
-        cols.add(new ArrayList<Card>());
-        cols.add(new ArrayList<Card>());
-    }
-
-    public void buildDeck() {
-        for(int i = 2; i < 15; i++){
-            deck.add(new Card(i,Suit.Clubs));
-            deck.add(new Card(i,Suit.Hearts));
-            deck.add(new Card(i,Suit.Diamonds));
-            deck.add(new Card(i,Suit.Spades));
-        }
-    }
-
-    public void shuffle() {
-        long seed = System.nanoTime();
-        Collections.shuffle(deck, new Random(seed));
+        columns.add(new Column(1));
+        columns.add(new Column(2));
+        columns.add(new Column(3));
+        columns.add(new Column(4));
     }
 
     public void dealFour() {
-        for(int i = 0; i < 4; i++){
-            cols.get(i).add(deck.get(deck.size()-1));
-            deck.remove(deck.size()-1);
+        ArrayList<Card> deal = deck.dealFour();
+        for (int i = 0; i < deal.size(); i++) {
+            Card c = deal.get(i);
+            columns.get(i).cards.add(c);
         }
-    }
-
-    //customDeal to setup game for testing purposes
-    public void customDeal(int c1, int c2, int c3, int c4) {
-        cols.get(0).add(deck.get(c1));
-        deck.remove(c1);
-        cols.get(1).add(deck.get(c2));
-        deck.remove(c2);
-        cols.get(2).add(deck.get(c3));
-        deck.remove(c3);
-        cols.get(3).add(deck.get(c4));
-        deck.remove(c4);
     }
 
     public void remove(int columnNumber) {
@@ -74,20 +39,20 @@ public class Game {
                 }
             }
             if (removeCard) {
-                this.cols.get(columnNumber).remove(this.cols.get(columnNumber).size() - 1);
+                this.columns.get(columnNumber).cards.remove(this.columns.get(columnNumber).cards.size() - 1);
             }
         }
     }
 
     private boolean columnHasCards(int columnNumber) {
-        if(this.cols.get(columnNumber).size()>0){
+        if (this.columns.get(columnNumber).cards.size()>0) {
             return true;
         }
         return false;
     }
 
     private Card getTopCard(int columnNumber) {
-        return this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1);
+        return this.columns.get(columnNumber).cards.get(this.columns.get(columnNumber).cards.size()-1);
     }
 
 
@@ -98,10 +63,10 @@ public class Game {
     }
 
     private void addCardToCol(int columnTo, Card cardToMove) {
-        cols.get(columnTo).add(cardToMove);
+        columns.get(columnTo).cards.add(cardToMove);
     }
 
     private void removeCardFromCol(int colFrom) {
-        this.cols.get(colFrom).remove(this.cols.get(colFrom).size()-1);
+        this.columns.get(colFrom).cards.remove(this.columns.get(colFrom).cards.size()-1);
     }
 }
